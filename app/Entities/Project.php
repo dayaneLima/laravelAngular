1da9a0200cb2edd783b5790936ec58e30923eb0b
dayane <dayane.lima.castro@gmail.com>
2015-08-27 21:34:02 -0300

    correção projeto fase 2
    correção projeto fase 2, faltou preencher os dados do fillable da model
    Project

--- app/Entities/Project.php
+++ app/Entities/Project.php
@@ -10,7 +10,15 @@ class Project extends Model implements Transformable
 {
     use TransformableTrait;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'owner_id',
+        'client_id',
+        'name',
+        'description',
+        'progress',
+        'status',
+        'due_date'
+    ];
 
     public function owner(){
         return $this->hasOne('CodeProject\Entities\User','id','owner_id');
